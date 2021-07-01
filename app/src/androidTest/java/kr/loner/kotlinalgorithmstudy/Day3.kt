20bae74f957e9a66fd4f44bbade0e8bdb45c4145
Yong <pyg941007@hanmail.net>
2021-07-01 22:21:32 +0900

    Day3 init

---
+++ app/src/androidTest/java/kr/loner/kotlinalgorithmstudy/Day3.kt
@@ -0,0 +1,25 @@
+package kr.loner.kotlinalgorithmstudy
+
+/**
+ * KotlinAlgorithmStudy
+ * Class: Day3
+ * Created by pyg10.
+ * Created On 2021-07-01.
+ * Description:
+ */
+
+fun main(){
+
+    val matrix = MatrixBorderRotation()
+    matrix.solution(6,6, arrayOf(intArrayOf(2,2,5,4), intArrayOf(3,3,6,6), intArrayOf(5,1,6,3)))
+
+}
+
+class MatrixBorderRotation{
+
+    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+
+}
