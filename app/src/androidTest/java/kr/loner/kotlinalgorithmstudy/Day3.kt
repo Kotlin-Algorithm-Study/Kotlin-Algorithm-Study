f0234f6abb8c9b1040a977ab200f25fa577e6480
Yong <pyg941007@hanmail.net>
2021-07-03 14:19:05 +0900

    옳은 괄호 추가

--- app/src/androidTest/java/kr/loner/kotlinalgorithmstudy/Day3.kt
+++ app/src/androidTest/java/kr/loner/kotlinalgorithmstudy/Day3.kt
@@ -1,5 +1,7 @@
 package kr.loner.kotlinalgorithmstudy
 
+import java.util.*
+
 /**
  * KotlinAlgorithmStudy
  * Class: Day3
@@ -13,6 +15,9 @@ fun main(){
     val matrix = MatrixBorderRotation()
     matrix.solution(3,3, arrayOf(intArrayOf(1,1,2,2), intArrayOf(1,2,2,3), intArrayOf(2,1,3,2),
          intArrayOf(2,2,3,3)))
+
+    val bracket = CorrectBracket()
+    bracket.solution("[](){}")
 
 }
 
@@ -96,4 +101,42 @@ class MatrixBorderRotation{
         return minResult.toIntArray()
     }
 
+}
+
+
+class CorrectBracket{
+    fun solution(s: String): Int {
+        var answer: Int = 0
+
+        for ( i in s.indices){ // 회전 몇개까지 시킬껀가
+            val value = s.toMutableList() // 리스트로 변환
+            for (j in 0 until i){ // 회전시킬 갯수
+                value.add(value[0]) // 처음것을 맨마지막에 삽입
+                value.removeAt(0) // 처음을 삽입해줬으니 제거
+            }
+
+            if (isCorrect(value)) // 옳은 괄호라면 카운트
+                answer++
+        }
+        return answer
+    }
+
+    private fun isCorrect(bracket: MutableList<Char>): Boolean{
+        val stack = Stack<Char>()
+
+        for (i in bracket.indices){
+            if (stack.isEmpty()) // 스택이 비어있다면 삽입
+                stack.push(bracket[i])
+            else{
+                when(bracket[i]){ // 비어있지않다면 } ] ) 의 경우 스택의내용과 비교한후 삽입하거나 꺼냄
+                    '}' -> if (stack.peek() == '{') stack.pop() else stack.push(bracket[i])
+                    ']' -> if (stack.peek() == '[') stack.pop() else stack.push(bracket[i])
+                    ')' -> if (stack.peek() == '(') stack.pop() else stack.push(bracket[i])
+                    else -> stack.push(bracket[i]) // } ] ) 외의경우는 삽입
+                }
+
+            }
+        }
+        return stack.isEmpty() // 비어있다면 옳은괄호 비어있지않다면 옳지않은 괄호
+    }
 }
